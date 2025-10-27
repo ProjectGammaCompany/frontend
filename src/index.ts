f47d1982b0a68a94f3b18ab20d8da0703ebcde2a
EvgenBolat <khavronich12@gmail.com>
2025-10-27 18:28:04 +0300

    feat: split ci & cd and add partially logger

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// import HawkCatcher from "@hawk.so/javascript";
+
+// const hawk = new HawkCatcher({
+//   token:
+//     "eyJpbnRlZ3JhdGlvbklkIjoiZDY4YWQ4N2UtOTg3OC00ODJkLTk2YmEtZjM3NjczOWNlMDczIiwic2VjcmV0IjoiNGNjNWZiZGMtNmJlNS00OGMxLTk4YmYtMmFmNmE3NGI3ZmQwIn0=",
+// });
