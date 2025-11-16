5ead11ed307ea64d8d1faa5e5e79a933103e83df
EvgenBolat <khavronich12@gmail.com>
2025-11-16 15:31:23 +0300

    fix: fix tests
    Исправлено название env файла, из-за чего в CI не проходили тесты, добавлен вывод env переменной в страницы авторизации, чтобы затем на проде можно было посмотреть работу env переменных.

--- src/pages/auth/ui/AuthPage.tsx
+++ src/pages/auth/ui/AuthPage.tsx
@@ -2,6 +2,7 @@ export const AuthPage = () => {
   return (
     <div>
       <h1>Страница авторизации</h1>
+      <h2>{import.meta.env.VITE_APP_BASE_URL}</h2>
       <button
         onClick={() => {
           console.log("Вход");
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,16 @@
 import react from "@vitejs/plugin-react";
+import { loadEnv } from "vite";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   test: {
     environment: "jsdom", // браузерная среда
     globals: true, // позволяет использовать глобальные функции типа `describe`, `it`
     setupFiles: "./src/jest.setup.ts", // можно оставить setup файл, например для RTL
-
+    env: loadEnv(mode, process.cwd(), ""),
     coverage: {
       provider: "v8",
     },
   },
-});
+}));
