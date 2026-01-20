3ec16ea5f52ae563bbd45cbdb8758d6295ff2ece
EvgenBolat <khavronich12@gmail.com>
2026-01-20 21:21:23 +0300

    fix: remove parallel block for mvp

--- src/entities/Block/ui/BlockCard/BlockCard.tsx
+++ src/entities/Block/ui/BlockCard/BlockCard.tsx
@@ -6,14 +6,21 @@ interface BlockCardProps {
   children?: ReactNode;
   draggableId: string;
   index: number;
+  isDragDisabled?: boolean;
 }
 
-const BlockCard = ({ children, draggableId, index }: BlockCardProps) => {
+const BlockCard = ({
+  children,
+  draggableId,
+  index,
+  isDragDisabled,
+}: BlockCardProps) => {
   return (
     <DraggableCard
       draggableId={draggableId}
       index={index}
       className="block-card"
+      isDragDisabled={isDragDisabled}
     >
       {children}
     </DraggableCard>
