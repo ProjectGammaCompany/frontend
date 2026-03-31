514706382a300ded57f611ac11bd6447d7792d14
EvgenBolat <khavronich12@gmail.com>
2026-03-31 16:05:51 +0300

    fix: fix role bug

--- src/entities/Event/model/useRole.ts
+++ src/entities/Event/model/useRole.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { getRole } from "../api/getRole";
+import { eventQueries } from "../api/queries";
 
 export const useRole = (eventId: string | undefined) => {
   return useQuery({
-    queryKey: ["userRole"],
+    queryKey: eventId ? eventQueries.getUserRole(eventId) : ["userRole"],
     queryFn: () => {
       if (eventId) {
         return getRole(eventId);
