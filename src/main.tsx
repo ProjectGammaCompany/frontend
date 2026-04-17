1ce3132a370d969e3e4aa9e17ca349fb084648a4
EvgenBolat <khavronich12@gmail.com>
2026-04-17 15:31:44 +0300

    feat: imrpove pwa
    проделана работа с service worker

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { registerSW } from "virtual:pwa-register";
 import App from "./app/App.tsx";
 import { store } from "./app/providers/reduxProvider/store.ts";
 import "./index.scss";
 import { queryClient } from "./shared/api/reactQuery/queryClient.ts";
+
+registerSW({ immediate: true });
 
 async function enableMocks() {
   if (import.meta.env.DEV && import.meta.env.VITE_ENABLE_MSW === "true") {
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import Sitemap from "vite-plugin-sitemap";
 import tsconfigPaths from "vite-tsconfig-paths";
-
 const excludedRoutes = [
   "/auth",
   "/forgot-password",
@@ -16,6 +16,51 @@ export default defineConfig({
     react({
       babel: {
         plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*{html,css,js,ico,png,svg}"],
+        maximumFileSizeToCacheInBytes: 3000000,
+      },
+      manifest: {
+        name: "EduPlay",
+        short_name: "EduPlay",
+        icons: [
+          {
+            src: "/img/favicon/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/img/favicon/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        scope: "/",
+        orientation: "any",
+        lang: "ru-RU",
+        start_url: "/home",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        screenshots: [
+          {
+            src: "/img/screenshots/desktop.png",
+            type: "image/png",
+            sizes: "2880x1564",
+            form_factor: "wide",
+          },
+          {
+            src: "/img/screenshots/mobile.png",
+            type: "image/png",
+            sizes: "narrow",
+          },
+        ],
+        display: "standalone",
       },
     }),
     tsconfigPaths(),
